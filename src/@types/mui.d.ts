e52eccd5dddbec47f328ffde68b500e489115b6a
Marco Morales <marco.bbcm@gmail.com>
2023-07-23 00:09:14 +0000

    Adds CssVarsTheme to Theme
    Adds CssVarsTheme to declaration file so
    that theme argument in sx function has the
    correct type.

--- src/@types/mui.d.ts
+++ src/@types/mui.d.ts
@@ -1,10 +1,13 @@
 import { PaletteColor, PaletteColorOptions, Theme } from "@mui/material/styles"
+import { CssVarsTheme } from "@mui/material"
 
 declare module "@mui/styles/defaultTheme" {
   interface DefaultTheme extends Theme { }
 }
 
 declare module "@mui/material/styles" {
+  interface Theme extends CssVarsTheme { }
+
   interface TypeBackground {
     paperLight: string
   }
