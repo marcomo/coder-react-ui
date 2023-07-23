37da43e371fda4978cd189b870910395b7ee6883
Marco Morales <marco.bbcm@gmail.com>
2023-07-23 19:09:21 +0000

    Fixes flash of color from light to dark
    Forces dark mode by setting the data attr
    in the html to dark

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,7 @@ import { Container } from '@mui/material';
 
 const App: React.FunctionComponent = () => {
   return (
-    /**
-     * In dev, data-mui-color-scheme="dark" is set here as a backup
-     * to defaultMode in App. This should NOT be used in production as the
-     * user would not be able to get out of dark mode. It is only including
-     * here should any dev running this code might have mode set in
-     * localStorage resulting in unwanted (incomplete) light-mode theme.
-     */
-    <Container disableGutters data-mui-color-scheme="dark">
+    <Container disableGutters>
       <Router />
     </Container>
   );
